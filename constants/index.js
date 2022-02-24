2d08779e891a488dcaa5f047475cf8a6e0bccdbe
wagolemusa <homiemusa@gmail.com>
2022-02-24 13:20:40 +0300

    hide env

--- constants/index.js
+++ constants/index.js
@@ -7,3 +7,6 @@ export const DOMAIN = process.env.APP_DOMIAN;
 export const SENDGRID_API = process.env.APP_SENDGRID_API;
 export const HOST_EMAIL = process.env.APP_HOST_EMAIL;
 export const PORT  = process.env.PORT || process.env.APP_PORT;
+
+
+// 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
